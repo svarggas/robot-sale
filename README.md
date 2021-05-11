47453e736aeebf083db2c8e741feb847da882f27
Sebastian Vargas Rojas <s.varggas@gmail.com>
2021-05-10 21:10:41 -0600

    Update - React Hooks

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
 # robo-sale
-Mini react app galery of robots in sale, powered by 2 API's:
+Trying React Hooks. 
+Galery of robots in sale, powered by 2 API's:
 1. :robot:,  https://robohash.org
 2. :bookmark_tabs:, https://jsonplaceholder.typicode.com
 
-To run the project:
-
-1. Clone this repo
-2. Run `npm install`
-3. Run `npm start`
+Visit
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,48 +1,45 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: [],
-      searchfield: ''
-    }
-  }
+function App() {
 
-  componentDidMount() {
+  // State with hooks
+  const [robots, setRobots] = useState([])
+  const [searchfield, setSearchfield] = useState('')
+
+  //Like ComponentDidMount, run once 
+  // Because of the second parameter being a empty array 
+  useEffect( () => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response=> response.json())
-      .then(users => {this.setState({ robots: users})});
+      .then(users => {setRobots(users)});
+  },[])
+
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value)
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
-  }
+  const filteredRobots = robots.filter(robot =>{
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  })
 
-  render() {
-    const { robots, searchfield } = this.state;
-    const filteredRobots = robots.filter(robot =>{
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    })
-    return !robots.length ? <h1>Loading</h1> :
-      (
-        <div className='tc'>
-          <h1 className='f1'>Robot Sale</h1>
-          <h3 className='f3' style={{ marginTop:'-30px' }}>
-            Dont miss a oportunity, prices <u>always</u> chaging, literally
-          </h3>
-          
-          <SearchBox searchChange={this.onSearchChange}/>
-          <Scroll>
-            <CardList robots={filteredRobots} />
-          </Scroll>
-        </div>
-      );
-  }
+  return !robots.length ? <h1>Loading</h1> :
+    (
+      <div className='tc'>
+        <h1 className='f1'>Robot Sale</h1>
+        <h3 className='f3' style={{ marginTop:'-30px' }}>
+          Dont miss a oportunity, prices <u>always</u> chaging, literally
+        </h3>
+        
+        <SearchBox searchChange={onSearchChange}/>
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
 }
 
 export default App;
